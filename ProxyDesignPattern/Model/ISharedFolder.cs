53c04ac1ac4d86be89bc4e0a66e10a84b935f33d
unknown <Bhagya>
2023-08-22 21:14:09 +0530

    committed

---
+++ ProxyDesignPattern/Model/ISharedFolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyDesignPattern.Model
+{
+    // The Subject interface declares common operations for both RealSubject and the Proxy. 
+    internal interface ISharedFolder
+    {
+        void PerformRWOperations();
+    }
+}
---
+++ ProxyDesignPattern/Model/SharedFolder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyDesignPattern.Model
+{
+    //The RealSubject contains some core business logic.
+    internal class SharedFolder : ISharedFolder
+    {
+        public void PerformRWOperations()
+        {
+            Console.WriteLine("Performing Read Write operation on the Shared Folder");
+        }
+
+    }
+}
